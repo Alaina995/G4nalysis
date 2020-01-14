fca40b7fa4f96f6d9edc546da33dacca592fcef2
Jacques Bonnand <jacques.bonnand@isep.fr>
2020-01-14 12:42:27 +0100

    page dashboard quesiment finie

--- Dash board/dashboard.php
+++ Dash board/dashboard.php
@@ -2,45 +2,48 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <link rel="stylesheet" href="Nav_bar verticale.css" />
+        <link rel="stylesheet" href="dashboard.css" />
         <title>Nav bar verticale</title>
     </head>
 
     <body>
 
-	<?php require('../Nav_Bar_Website/nav_bar_principale.inc.html'); ?>
+	
         
         
 	<section class="features ">
                 <figure>
+					<a class="click_box" href="#">
+						Ceci est le test de température superficielle du corps.<br/>Cliquer pour lancer le test
+					</a>
                     <img src="images_projet/case_temper.png" alt="Tester ton Température">
-                    <figcaption id="températe">Températe</figcaption>
-                    <p id="températe1">37°C</p>
+                    <figcaption id="temperature">Température</figcaption>
+                    <p id="temperature1">37<a class="unite">°C</a></p>
                 </figure>
                 <figure>
                         <img src="images_projet/case_audit.png" alt="Tester audition">
                         <figcaption  id="audition">Audition</figcaption>
-                        <p id="audition1">65% Précis</p>
+                        <p id="audition1">65<a class="unite">%</a></p>
                      </figure>
                      <figure>
                             <img src="images_projet/case_coeur.png" alt="Tester la fréquence de coeur">
                             <figcaption id="coeur">Coeur</figcaption>
-                            <p  id="coeur1">75 Bpm</p>
+                            <p  id="coeur1">75<a class="unite">Bpm</a></p>
                         </figure>
                         <figure>
-                            <img src="images_projet/case_visu.png" alt="Tester la Visual">
-                            <figcaption  id="visual">Visual</figcaption>
-                            <p id="visual1">360ms</p>
+                            <img src="images_projet/case_visu.png" alt="Tester la vue">
+                            <figcaption  id="vue">Vue</figcaption>
+                            <p id="vue1">.36<a class="unite">s</a></p>
                         </figure>
                         <figure>
-                            <img src="images_projet/case_freq.png" alt="Tester la Entendu auditif">
-                            <figcaption  id="étendu">Étendu auditif</figcaption>
-                            <p id="étendu1">60%</p>
+                            <img src="images_projet/case_freq.png" alt="Tester l'étendue auditive">
+                            <figcaption  id="étendue">Étendue auditive</figcaption>
+                            <p id="étendue1">60<a class="unite">%</a></p>
                         </figure>
         </section>
 
-    	
-
+    	<?php require('../Nav_Bar_Website/nav_bar_principale.inc.html'); ?>
+<!-- 
     	<ul class="nav_bar_horizon">
 
 
@@ -50,7 +53,7 @@
     		<li id="logo_li"><img  id="nav_logo" src="G4analysis.png"/></li>
     		
         </ul>
-        
+         -->
 
 
     </body>
