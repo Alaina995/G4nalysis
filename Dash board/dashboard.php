1d00b3cde59feffc3ca148a12fd1105ca7a66985
Jacques Bonnand <jacques.bonnand@isep.fr>
2020-01-14 12:55:06 +0100

    page Dashborad encore améliorée

--- Dash board/dashboard.php
+++ Dash board/dashboard.php
@@ -13,32 +13,49 @@
         
 	<section class="features ">
                 <figure>
-					<a class="click_box" href="#">
-						Ceci est le test de température superficielle du corps.<br/>Cliquer pour lancer le test
-					</a>
+					<a class="lancer_test">Cliquer pour lancer le test</a>
                     <img src="images_projet/case_temper.png" alt="Tester ton Température">
                     <figcaption id="temperature">Température</figcaption>
-                    <p id="temperature1">37<a class="unite">°C</a></p>
+					<p id="temperature1">37<a class="unite">°C</a></p>
+					<a class="click_box" href="#">
+						Ceci est le test de température superficielle du corps.
+					</a>
                 </figure>
                 <figure>
+						<a class="lancer_test">Cliquer pour lancer le test</a>
                         <img src="images_projet/case_audit.png" alt="Tester audition">
                         <figcaption  id="audition">Audition</figcaption>
-                        <p id="audition1">65<a class="unite">%</a></p>
+						<p id="audition1">65<a class="unite">%</a></p>
+						<a class="click_box" href="#">
+							Ceci est le test d'audition.
+						</a>
                      </figure>
                      <figure>
+					 		<a class="lancer_test">Cliquer pour lancer le test</a>
                             <img src="images_projet/case_coeur.png" alt="Tester la fréquence de coeur">
                             <figcaption id="coeur">Coeur</figcaption>
-                            <p  id="coeur1">75<a class="unite">Bpm</a></p>
+							<p  id="coeur1">75<a class="unite">Bpm</a></p>
+							<a class="click_box" href="#">
+								Ceci est le test de fréquence cardiaque.
+							</a>
                         </figure>
                         <figure>
+							<a class="lancer_test">Cliquer pour lancer le test</a>
                             <img src="images_projet/case_visu.png" alt="Tester la vue">
                             <figcaption  id="vue">Vue</figcaption>
-                            <p id="vue1">.36<a class="unite">s</a></p>
+							<p id="vue1">.36<a class="unite">s</a></p>
+							<a class="click_box" href="#">
+								Ceci est le test de vue / réflexes.
+							</a>
                         </figure>
                         <figure>
+							<a class="lancer_test">Cliquer pour lancer le test</a>
                             <img src="images_projet/case_freq.png" alt="Tester l'étendue auditive">
                             <figcaption  id="étendue">Étendue auditive</figcaption>
-                            <p id="étendue1">60<a class="unite">%</a></p>
+							<p id="étendue1">60<a class="unite">%</a></p>
+							<a class="click_box" href="#">
+								Ceci est le test d'étendue auditive.
+							</a>
                         </figure>
         </section>
 
