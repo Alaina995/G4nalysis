8808e3ec93fc0f109277a49caf072e6296a2c678
Nad70 <59644169+Nad70@users.noreply.github.com>
2020-01-15 10:44:03 +0100

    Test Javascript

--- Accueil/vraie_page_accueil.php
+++ Accueil/vraie_page_accueil.php
@@ -37,15 +37,7 @@
 
 <script type="text/javascript">
  
- function toggle_text() {
-  var span = document.getElementById("span_txt");
-  if(span.innerHTML != "") {
-    span.innerHTML = "";
-  } else {
-    span.innerHTML = "Nous sommes une jeune start-up crée en 2018 et nous développons des solutions techniques pour nos clients. Nous sommes composée de 6 collaborateurs et l’année dernière nous avons réalisé un chiffre d’affaire de 500.000€.";
-  }
-}
-</script>    
+ <script src=".../Accueil/anim.js"> </script>   
 
 
     </body>
