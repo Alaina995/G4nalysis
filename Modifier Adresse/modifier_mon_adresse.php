ed4c3167ee0ea9d881a80db8243947492979a40b
Jacques Bonnand <jacques.bonnand@isep.fr>
2020-01-15 11:22:04 +0100

    inscription

--- Modifier Adresse/modifier_mon_adresse.php
+++ Modifier Adresse/modifier_mon_adresse.php
@@ -104,15 +104,6 @@
             <p> </p>
 		</nav>
 
-		<footer>
-			<div class="footer">
-                    <li><a href="Mentions_legales.html"> Mentions légales</a></li>
-                    <li><a href="qui_sommes_nous.html"> Qui sommes-nous?</a></li>
-                    <li><a href="FAQ.html"> Questions fréquentes</a></li>
-                    <li><a href="nous_contacter.html"> Nous contacter</a></li>
-            </div>
-		</footer>
-
 
     <?php $title = 'modifier mon adresse'; ?>
     <?php $currentPage = 'modifier mon adresse'; ?>
