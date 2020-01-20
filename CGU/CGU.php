770bfa78a6f6e20db3323c0335161b4646149fad
Alaina995 <59582135+Alaina995@users.noreply.github.com>
2020-01-20 23:28:36 +0100

    CGU

--- CGU/CGU.php
+++ CGU/CGU.php
@@ -2,13 +2,28 @@
     <head>
         <meta charset="utf-8" />
         <link rel="stylesheet" href="CGU.css" />
-        <title> Inscription </title>
+        <title> CGU </title>
     </head>
 
     <body>
 		<h1> Conditions générales d'utilisations </h1>
 
+        <h1> Conditions générales d'utilisation de G4nalysis </h1>
 
+        <h2>Article 1: OBJET</h2>
+        <p>Texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte</p>
+
+        <h3>Article 2: MENTIONS LEGALES</h3>
+        <p>Texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte</p>
+
+        <h4>Article 3: ACCES AUX SERVICES</h4>
+        <p>Texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte</p>
+
+        <h5>Article 5: ACCES AU SITE</h5>
+        <p>Texte texte texte texte texte texte texte texte texte texte texte texte texte texte </p>
+
+        <h6>Article 6: CONDITIONS GENERALES DE VENTE</h6>
+        <p>Texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte </p>
 
 
 		<?php require('../Footer/footer.php'); ?>
