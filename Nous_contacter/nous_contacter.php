7385e8ba4aa40fa27ebde12812f04c39b353eae7
Alaina995 <59582135+Alaina995@users.noreply.github.com>
2020-01-21 18:07:34 +0100

    Grosse PLS

--- Nous_contacter/nous_contacter.php
+++ Nous_contacter/nous_contacter.php
@@ -7,7 +7,6 @@
 	</head>
 
 	<form id="contact" method="post" action="traitement_formulaire.php">
-        <img  id="wave" src="Waves.png"/>
         <img  id="logo" src="G4analysis.png"/>
         
         <a id = "retouracc" href="../Accueil/vraie_page_accueil.php"><< Retour à l'accueil
@@ -16,6 +15,8 @@
         <p class = "titre">Nous contacter</p>
 
         <nav>
+            <img  id="care" src="4Care.png"/>
+            <img  id="barre" src ="Barre_separation.png"/>
 
             <form >
                 <p>
@@ -27,6 +28,8 @@
                     <br />
 
                     <label>Message</label><textarea id="message" name="message" rows="4" placeholder="Ecrivez votre message ici..."/ required required size ="30"></textarea><br />
+
+                    <input class="button" type="submit" id ="envoi" name="envoi" value="Envoyer votre message" />
                 </p>
             </form>
                 
