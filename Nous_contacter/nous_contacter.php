560b0a68b1afcfe706f969bc4145203e6afa7382
Alaina995 <59582135+Alaina995@users.noreply.github.com>
2020-01-21 18:04:02 +0100

    Grosse PLS

--- Nous_contacter/nous_contacter.php
+++ Nous_contacter/nous_contacter.php
@@ -5,27 +5,31 @@
 		<link rel="stylesheet" href="nous_contacter.css"/>
 		<?php session_start();?>
 	</head>
-	<body>
-			<img  id="wave" src="Waves.png"/>
-        	<img  id="logo" src="G4analysis.png"/>
-			<h1>Nous contacter</h1>
 
+	<form id="contact" method="post" action="traitement_formulaire.php">
+        <img  id="wave" src="Waves.png"/>
+        <img  id="logo" src="G4analysis.png"/>
+        
+        <a id = "retouracc" href="../Accueil/vraie_page_accueil.php"><< Retour à l'accueil
+        </a>
 
-			    <fieldset>
-				<form id="contact" method="post" action="traitement_formulaire.php">
+        <p class = "titre">Nous contacter</p>
 
-					<label>Nom</label><input class="input" type="text" id="nom"
+        <nav>
+
+            <form >
+                <p>
+                    <label>Nom</label><input class="input" type="text" id="nom"
 			        name="nom" placeholder="Quel est votre nom?"/ required size ="30"/><br />
-
-                	<label>Email</label><input class="input" type="email"id="email" name="email" placeholder="votre_email@email.com"/ required size ="30"/>
+			        
+                    <label>Email</label><input class="input" type="email"id="email" name="email" placeholder="votre_email@email.com"/ required size ="30"/>
                
                     <br />
 
                     <label>Message</label><textarea id="message" name="message" rows="4" placeholder="Ecrivez votre message ici..."/ required required size ="30"></textarea><br />
-
-                <input class="button" type="submit" id ="envoi" name="envoi" value="Envoyer votre message" />
-                </fieldset>
+                </p>
             </form>
-
+                
+        </nav>
 	</body>
 </html>
