d562c4f37218f584196a4c0b8c17863ca06aaaf3
Alaina995 <59582135+Alaina995@users.noreply.github.com>
2020-01-21 02:33:13 +0100

    inscription_php1

--- Inscription/postinscription.php
+++ Inscription/postinscription.php
@@ -47,3 +47,118 @@ if($AfficherFormulaire==1){
     name="nom de l'input" sert à le reconnaitre une fois le bouton submit cliqué, pour le code PHP
      -->
 }
+
+<?php 
+    session_start();
+
+    if(empty($_POST['nom'])){
+        $_SESSION['nom'] ="Merci de remplir votre nom";
+    }
+
+    else{
+        $_SESSION['nom'] = "";
+        setcookie('nom',$_POST['nom'], time() + (90000 *30), "/");
+    }
+
+    if(empty($_POST['prenom'])){
+        $_SESSION['prenom'] ="Merci de remplir votre prenom";
+    }
+
+    else{
+        $_SESSION['prenom'] = "";
+        setcookie('prenom',$_POST['prenom'], time() + (90000 *30), "/");
+    }
+
+    if(empty($_POST['email']) OR ! filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+        $_SESSION['email'] ="Merci de remplir votre email";
+    }
+
+    else{
+        $_SESSION['email'] = "";
+        setcookie('email',$_POST['email'], time() + (90000 *30), "/");
+    }
+
+    if(empty($_POST['emailConfirmation']) OR ! filter_var($_POST['emailConfirmation'], FILTER_VALIDATE_EMAIL)){
+        $_SESSION['emailConfirmation'] ="Merci de remplir votre email de confirmation";
+    }
+
+    else{
+        $_SESSION['emailConfirmation'] = "";
+        setcookie('emailConfirmation',$_POST['emailConfirmation'], time() + (90000 *30), "/");
+    }
+
+    if(empty($_POST['emailConfirmation']) OR ! filter_var($_POST['emailConfirmation'], FILTER_VALIDATE_EMAIL)){
+        $_SESSION['emailConfirmation'] ="Merci de remplir votre email de confirmation";
+    }
+
+    else{
+        $_SESSION['emailConfirmation'] = "";
+        setcookie('emailConfirmation',$_POST['emailConfirmation'], time() + (90000 *30), "/");
+    }
+
+    if(empty($_POST['motdepasse'])){
+        $_SESSION['motdepasse'] ="Merci de remplir votre mot de passe";
+    }
+
+    else{
+        $_SESSION['motdepasse'] = "";
+        setcookie('motdepasse',$_POST[''], time() + (90000 *30), "/");
+    }
+
+    if(empty($_POST['motdepasseConfirmation'])){
+        $_SESSION['motdepasseConfirmation'] ="Merci de confirmer votre mot de passe";
+    }
+
+    else{
+        $_SESSION['motdepasseConfirmation'] = "";
+        setcookie('motdepasseConfirmation',$_POST[''], time() + (90000 *30), "/");
+    }
+
+    if(empty($_POST['Ville'])){
+        $_SESSION['Ville'] ="Merci de remplir votre Ville";
+    }
+
+    else{
+        $_SESSION['codepo'] = "";
+        setcookie('codepo',$_POST[''], time() + (90000 *30), "/");
+    }
+
+    if(empty($_POST[''])){
+        $_SESSION[''] ="Merci de remplir votre code postale";
+    }
+
+    else{
+        $_SESSION['codepo'] = "";
+        setcookie('codepo',$_POST[''], time() + (90000 *30), "/");
+    }
+
+    if(empty($_POST['rue'])){
+        $_SESSION['rue'] ="Merci de remplir votre rue";
+    }
+
+    else{
+        $_SESSION['rue'] = "";
+        setcookie('rue',$_POST[''], time() + (90000 *30), "/");
+    }
+
+    if(empty($_POST['nrue'])){
+        $_SESSION['nrue'] ="Merci de remplir votre numero de rue";
+    }
+
+    else{
+        $_SESSION['nrue'] = "";
+        setcookie('nrue',$_POST[''], time() + (90000 *30), "/");
+    }
+
+
+    if (empty($_POST['nom'])n||empty($_POST['prenom'])||empty($_POST['email'])||empty($_POST['emailConfirmation'])||empty($_POST['motdepasse'])||empty($_POST['motdepasseConfirmation'])||empty($_POST['Ville'])||empty($_POST['codepo'])||empty($_POST['rue'])||empty($_POST['nrue'])||){
+
+        header("Location:index.php");
+    }
+
+    else{
+        echo"Vous avez bien rempli le formulaire!";
+    session_destroy();
+    }
+?>
+
