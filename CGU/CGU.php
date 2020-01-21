1e8269c1024ac9b42a0ae65329995127880d1cba
Alaina995 <59582135+Alaina995@users.noreply.github.com>
2020-01-21 12:39:51 +0100

    cgu

--- CGU/CGU.php
+++ CGU/CGU.php
@@ -28,10 +28,5 @@
 
 		<?php require('../Footer/footer.php'); ?>
 
-    	<?php $title = 'Profil'; ?>
-		<?php $currentPage = 'profil'; ?>
-    	<?php require('../Nav_Bar_Website/nav_bar_principale.inc.php'); ?>
-
-
 	</body>
 </html>
